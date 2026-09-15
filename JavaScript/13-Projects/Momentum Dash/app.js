e76ff67c905cf96d0b58a94a4672f2aeea1d4352
Shakir Aziz <shakiraziz686@gmail.com>
2026-09-15 15:38:21 +0500

    Add: Js sturcture

---
+++ JavaScript/13-Projects/Momentum Dash/app.js
@@ -0,0 +1,18 @@
+/* ------ Constants ------ */
+
+/* ------ State ------ */
+
+/* ------ DOM Elements ------ */
+const weatherIcon = document.getElementById("weather-icon");
+const weatherTemp = document.getElementById("weather-temp");
+const weatherCity = document.getElementById("weather-city");
+
+/* ------ Helper Functions ------ */
+
+/* ------ Event Listeners ------ */
+
+/* ------ Event Handlers ------ */
+
+/* ------ HTML ------ */
+
+/* ------ Render ------ */
